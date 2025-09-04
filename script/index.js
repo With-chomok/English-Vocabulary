1ec8d05ae99de36cc3d7bd8d202c2f8371dbc5ec
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-09-04 17:49:42 +0600

    lesson container and cards added

---
+++ script/index.js
@@ -0,0 +1,49 @@
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((lesson) => displayLesson(lesson.data));
+};
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+const displayLevelWord = (words) => {
+  const wordContainer =document.getElementById("word-container")
+  wordContainer.innerHTML = "";
+
+  words.forEach(word => {
+    
+    
+    const card = document.createElement("div");
+    card.innerHTML = `
+     <div class="bg-white rounded-xl shadow-md text-center space-y-4 py-10 px-5">
+        <h2 class="text-2xl font-bold">${word.word}</h2>
+        <p class="font-semibold ">Meaning /Pronounciation</p>
+        <div class="font-medium text-2xl text-[#18181B] font-bangla">${word.meaning}/${word.pronunciation}</div>
+        <div class="flex justify-between  items-center ">
+          <button class="btn bg-[#1a91ff1a]  hover:bg-primary  hover:text-white"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-[#1a91ff1a] hover:bg-primary hover:text-white" ><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>
+    `
+    wordContainer.append(card)
+  })
+};
+const displayLesson = (lessons) => {
+  const levelContainer = document.getElementById("level-container");
+
+  levelContainer.innerHTML = "";
+
+  lessons.forEach((lesson) => {
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+    <button href="" onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"
+          ><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>`;
+
+    levelContainer.append(btnDiv);
+  });
+};
+loadLessons();
